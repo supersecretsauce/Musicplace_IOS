f9b220729cd4cd1adf178596cbe7e2faf05ad1fe
Max Mandia <mmandia511@gmail.com>
2022-07-21 16:13:36 -0400

    got some shit working finally

--- src/screens/post/test.js
+++ src/screens/post/test.js
@@ -10,6 +10,7 @@ import firestore from '@react-native-firebase/firestore';
 import {firebase} from '@react-native-firebase/firestore';
 import Colors from '../../assets/utilities/Colors';
 import {authorize} from 'react-native-app-auth';
+import {authFetch} from '../../services/SpotifyService';
 import axios from 'axios';
 import {Buffer} from 'buffer';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -24,22 +25,6 @@ const TestScreen = () => {
   const spotifyRefreshURL = 'https://accounts.spotify.com/api/token';
   const [accessToken, setAccessToken] = useState('');
   const [refreshToken, setRefreshToken] = useState('');
-
-  const spotConfig = {
-    clientId: '501638f5cfb04abfb61d039e370c5d99', // available on the app page
-    clientSecret: '16f92a6d7e9a4180b29af25bf012e6fe', // click "show client secret" to see this
-    redirectUrl: 'musicplace-ios:/musicplace-ios-login', // the redirect you defined after creating the app
-    scopes: [
-      'user-read-email',
-      'playlist-modify-public',
-      'user-read-private',
-      'user-library-read',
-    ], // the scopes you need to access
-    serviceConfiguration: {
-      authorizationEndpoint: 'https://accounts.spotify.com/authorize',
-      tokenEndpoint: 'https://accounts.spotify.com/api/token',
-    },
-  };
 
   // check if user has spotify connected to display proper screens
   useEffect(() => {
@@ -60,77 +45,14 @@ const TestScreen = () => {
     checkForSpotifyConnection();
   }, []);
 
-  // interceptors
-
-  const authFetch = axios.create({
-    baseURL: 'https://api.spotify.com/v1/',
-    headers: {
-      Authorization: 'Bearer ' + accessToken,
-      'Content-Type': 'application/json',
-    },
-  });
-
-  // Add a request interceptor
-  authFetch.interceptors.request.use(
-    function (config) {
-      // Do something before request is sent
-      console.log('request sent');
-      return config;
-    },
-    function (error) {
-      // Do something with request error
-      console.log('request error');
-      return Promise.reject(error);
-    },
-  );
-
-  // Add a response interceptor
-  authFetch.interceptors.response.use(
-    function (response) {
-      // Any status code that lie within the range of 2xx cause this function to trigger
-      // Do something with response data
-      console.log('got response');
-      return response;
-    },
-    function (error) {
-      // Any status codes that falls outside the range of 2xx cause this function to trigger
-      // Do something with response error
-      console.log('response error');
-      if (error.response.status === 401) {
-        const data = qs.stringify({
-          grant_type: 'refresh_token',
-          refresh_token: refreshToken,
-        });
-        axios
-          .post(spotifyRefreshURL, data, {
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              Authorization:
-                'Basic ' +
-                Buffer.from(
-                  spotConfig.clientId + ':' + spotConfig.clientSecret,
-                ).toString('base64'),
-            },
-          })
-          .then(response => {
-            console.log(response.data.access_token);
-            setAccessToken(response.data.access_token);
-            setRefreshToken(response.data.refresh_token);
-          })
-          .catch(e => {
-            console.log(e);
-            console.log('bad token?');
-          });
-      }
-      // getRefreshToken();
-
-      return Promise.reject(error);
-    },
-  );
-
   const FetchData = async () => {
     try {
-      const resp = await authFetch.get('/me');
+      const resp = await authFetch(
+        accessToken,
+        refreshToken,
+        setAccessToken,
+        setRefreshToken,
+      ).get('/me');
       console.log(resp);
     } catch (error) {
       console.log(error);
---
+++ src/services/SpotifyService.js
@@ -0,0 +1,83 @@
+const spotConfig = {
+  clientId: '501638f5cfb04abfb61d039e370c5d99', // available on the app page
+  clientSecret: '16f92a6d7e9a4180b29af25bf012e6fe', // click "show client secret" to see this
+  redirectUrl: 'musicplace-ios:/musicplace-ios-login', // the redirect you defined after creating the app
+  scopes: [
+    'user-read-email',
+    'playlist-modify-public',
+    'user-read-private',
+    'user-library-read',
+  ], // the scopes you need to access
+  serviceConfiguration: {
+    authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+    tokenEndpoint: 'https://accounts.spotify.com/api/token',
+  },
+};
+import axios from 'axios';
+import {Buffer} from 'buffer';
+const qs = require('qs');
+
+// axios instance
+
+export const authFetch = (
+  accessToken,
+  refreshToken,
+  setAccessToken,
+  setRefreshToken,
+) => {
+  const axiosInstance = axios.create({
+    baseURL: 'https://api.spotify.com/v1',
+    headers: {
+      Authorization: 'Bearer ' + accessToken,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  axiosInstance.interceptors.response.use(
+    function (response) {
+      console.log('got response');
+      return response;
+    },
+    function (error) {
+      console.log('response error');
+      if (error.response.status === 401) {
+        const data = qs.stringify({
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken,
+        });
+        axios
+          .post('https://accounts.spotify.com/api/token', data, {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              Authorization:
+                'Basic ' +
+                Buffer.from(
+                  spotConfig.clientId + ':' + spotConfig.clientSecret,
+                ).toString('base64'),
+            },
+          })
+          .then(response => {
+            console.log('set new tokens!');
+            setAccessToken(response.data.access_token);
+            setRefreshToken(response.data.refresh_token);
+          })
+          .catch(e => {
+            console.log(e);
+            console.log('bad token?');
+          });
+      }
+      return Promise.reject(error);
+    },
+  );
+  return axiosInstance;
+};
+
+//   const FetchData = async () => {
+//     try {
+//       const resp = await authFetch.get('/me');
+//       console.log(resp);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+// };
