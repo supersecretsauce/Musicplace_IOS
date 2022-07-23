cb713e6c1f1fcd2ebfc939c4775e244b34087c8c
Max Mandia <mmandia511@gmail.com>
2022-07-23 00:59:12 -0400

    Feat: added playlist names

--- src/components/SpotifyPlaylists.js
+++ src/components/SpotifyPlaylists.js
@@ -12,8 +12,8 @@ import {useEffect, useState} from 'react';
 const SpotifyPlaylists = props => {
   const playlists = props.playlists;
   const [cleanPlaylists, setCleanPlaylists] = useState(null);
+  const [cleanPlaylistInfo, setCleanPlaylistInfo] = useState(null);
   const [eligiblePlaylistArray, setEligiblePlaylistArray] = useState(null);
-  const [playlistURL, setPlaylistURL] = useState(null);
 
   useEffect(() => {
     if (playlists) {
@@ -26,44 +26,48 @@ const SpotifyPlaylists = props => {
       setEligiblePlaylistArray(
         cleanPlaylists.filter(playlist => playlist !== undefined),
       );
+      setCleanPlaylistInfo(
+        playlists.filter(playlist => playlist?.images[0] !== undefined),
+      );
     }
-  }, [cleanPlaylists]);
+  }, [cleanPlaylists, playlists]);
 
-  useEffect(() => {
-    console.log(eligiblePlaylistArray);
-  }, [playlistURL, eligiblePlaylistArray]);
+  //   useEffect(() => {
+  //     if (cleanPlaylistInfo) {
+  //       console.log(cleanPlaylistInfo);
+  //     }
+  //   }, [cleanPlaylistInfo]);
 
   return (
     <View>
       {eligiblePlaylistArray ? (
         <>
-          {/* <ScrollView numColumn={2} style={styles.scrollContainer}>
-            {eligiblePlaylistArray.map(playlist => {
-              return (
-                <View style={styles.playlistContainer} key={playlist.url}>
+          <View style={styles.scrollContainer}>
+            <FlatList
+              numColumns={2}
+              data={cleanPlaylistInfo}
+              renderItem={({item, index}) => (
+                <View style={styles.photoContainer} key={index}>
                   <Image
                     style={styles.playlistPhotos}
                     source={{
-                      uri: playlist.url,
+                      uri: item.images[0]?.url,
                     }}
                   />
+                  <Text style={styles.text}>{item.name}</Text>
                 </View>
+              )}
+            />
+            {/* {cleanPlaylistInfo.map(playlist => {
+              return (
+                <>
+                  <View key={playlist.id}>
+                    <Text style={styles.text}>{playlist.name}</Text>
+                  </View>
+                </>
               );
-            })}
-          </ScrollView> */}
-          <FlatList
-            data={eligiblePlaylistArray}
-            renderItem={({item, index}) => (
-              <View key={index}>
-                <Image
-                  style={styles.playlistPhotos}
-                  source={{
-                    uri: item.url,
-                  }}
-                />
-              </View>
-            )}
-          />
+            })} */}
+          </View>
         </>
       ) : null}
     </View>
@@ -77,10 +81,12 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   scrollContainer: {
-    marginLeft: '4%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  playlistContainer: {
-    marginTop: '6%',
+  photoContainer: {
+    padding: 19,
+    marginTop: '5%',
   },
   playlistPhotos: {
     height: 160,
