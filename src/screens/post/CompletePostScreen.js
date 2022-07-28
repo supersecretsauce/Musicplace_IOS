fa3a589bda5ed2491f65ab5a447e9ba15e729833
Max Mandia <mmandia511@gmail.com>
2022-07-27 22:52:45 -0400

    almost done

--- src/screens/post/CompletePostScreen.js
+++ src/screens/post/CompletePostScreen.js
@@ -4,7 +4,10 @@ import {
   View,
   SafeAreaView,
   TouchableWithoutFeedback,
+  TouchableOpacity,
   Image,
+  TextInput,
+  Switch,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import Colors from '../../assets/utilities/Colors';
@@ -13,7 +16,8 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 const CompletePostScreen = ({route, navigation}) => {
   const {song, songPhoto} = route.params;
   const [songIMG, setSongIMG] = useState();
-  console.log(song);
+  const [value, setValue] = useState(false);
+  // console.log(song);
   useEffect(() => {
     if (songPhoto) {
       setSongIMG(songPhoto);
@@ -29,6 +33,7 @@ const CompletePostScreen = ({route, navigation}) => {
           <View style={styles.postContainer}>
             <View style={styles.topContainer}>
               <Ionicons
+                onPress={() => navigation.navigate('PostASongScreen')}
                 style={styles.chevron}
                 name="chevron-back"
                 color="white"
@@ -45,18 +50,91 @@ const CompletePostScreen = ({route, navigation}) => {
               }}
             />
             <View style={styles.songInfoContainer}>
-              <Text style={styles.songName}>{song.name}</Text>
-              <View style={styles.artistContainer}>
+              <Text numberOfLines={1} style={styles.songName}>
+                {song.name}
+              </Text>
+              <View numberOfLines={1} style={styles.artistContainer}>
                 <Text style={styles.byText}>by </Text>
-                <Text style={styles.artists}>
+                <Text numberOfLines={1} style={styles.artists}>
                   {song.artists.map(artist => artist.name).join(', ')}
                 </Text>
               </View>
-              <View style={styles.albumContainer}>
+              <View numberOfLines={1} style={styles.albumContainer}>
                 <Text style={styles.byText}>from </Text>
-                <Text style={styles.albumName}>{song.album.name}</Text>
+                <Text numberOfLines={1} style={styles.albumName}>
+                  {song.album.name}
+                </Text>
               </View>
+              <TouchableOpacity style={styles.previewBtn}>
+                <Ionicons
+                  // style={styles.chevron}
+                  name="play-circle"
+                  color="white"
+                  size={18}
+                />
+                <Text style={styles.previewText}>Preview</Text>
+              </TouchableOpacity>
             </View>
+          </View>
+          <View style={styles.captionContainer}>
+            <TextInput
+              // eslint-disable-next-line react-native/no-inline-styles
+              style={{
+                fontFamily: 'Inter-Regular',
+                fontSize: value ? 16 : 16,
+                color: value ? 'white' : 'white',
+              }}
+              maxLength={162}
+              multiline
+              autoCapitalize="none"
+              keyboardType="default"
+              placeholder="Caption ideas include things like: where you were you first heard this song, the people you play this song with the most...idk we already ran out of ideas. "
+              placeholderTextColor="grey"
+              value={value}
+            />
+          </View>
+          <View style={styles.shareToContainer}>
+            <Text style={styles.shareTo}>Automatically share to</Text>
+            <View style={styles.socialRowContainer}>
+              <View style={styles.socialLeftContainer}>
+                <Ionicons
+                  style={styles.socialIcon}
+                  name="chatbubble"
+                  color="#53D769"
+                  size={24}
+                />
+                <Text style={styles.socials}>iMessage</Text>
+              </View>
+              <Switch style={{transform: [{scaleX: 0.8}, {scaleY: 0.8}]}} />
+            </View>
+            <View style={styles.socialRowContainer}>
+              <View style={styles.socialLeftContainer}>
+                <Ionicons
+                  style={styles.socialIcon}
+                  name="logo-twitter"
+                  color="#1D9BF0"
+                  size={24}
+                />
+                <Text style={styles.socials}>Twitter</Text>
+              </View>
+              <Switch style={{transform: [{scaleX: 0.8}, {scaleY: 0.8}]}} />
+            </View>
+            <View style={styles.socialRowContainer}>
+              <View style={styles.socialLeftContainer}>
+                <Ionicons
+                  style={styles.socialIcon}
+                  name="logo-snapchat"
+                  color="#FFFC00"
+                  size={24}
+                />
+                <Text style={styles.socials}>Snapchat</Text>
+              </View>
+              <Switch style={{transform: [{scaleX: 0.8}, {scaleY: 0.8}]}} />
+            </View>
+            <TouchableOpacity style={styles.postBtn}>
+              <Ionicons name="earth" color="white" size={24} />
+              <Text style={styles.postBtnText}>Post</Text>
+            </TouchableOpacity>
           </View>
         </View>
       )}
@@ -70,6 +148,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
+    alignItems: 'center',
   },
   postContainer: {
     backgroundColor: Colors.lightBlack,
@@ -88,29 +167,37 @@ const styles = StyleSheet.create({
     position: 'absolute',
     color: 'white',
     fontFamily: 'Inter-Semibold',
-    fontSize: 22,
+    fontSize: 28,
   },
 
   trackContainer: {
     marginTop: '10%',
     flexDirection: 'row',
+    justifyContent: 'center',
+    // alignItems: 'center',
   },
   songIMG: {
-    height: 140,
-    width: 140,
+    height: 165,
+    width: 165,
+    marginRight: '3%',
   },
   songInfoContainer: {
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'space-between',
+    marginLeft: '3%',
+    width: 165,
+    // backgroundColor: 'red',
   },
   songName: {
     color: 'white',
     fontFamily: 'Inter-bold',
-    fontSize: 30,
+    fontSize: 24,
+    maxWidth: 165,
   },
   artistContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'baseline',
+    maxWidth: 165,
   },
   byText: {
     color: 'white',
@@ -119,16 +206,98 @@ const styles = StyleSheet.create({
   },
   artists: {
     color: 'white',
-    fontFamily: 'Inter-Medium',
-    fontSize: 14,
+    fontFamily: 'Inter-bold',
+    fontSize: 17,
   },
   albumContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'baseline',
+    maxWidth: 165,
   },
   albumName: {
     color: 'white',
+    fontFamily: 'Inter-bold',
+    fontSize: 17,
+    maxWidth: 140,
+  },
+  previewBtn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: Colors.greyBtn,
+    borderRadius: 9,
+    paddingVertical: 4,
+    // paddingHorizontal: '100%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    width: 165,
+  },
+  previewText: {
+    color: 'white',
     fontFamily: 'Inter-Medium',
-    fontSize: 14,
+    fontSize: 16,
+    marginLeft: 5,
+  },
+  captionContainer: {
+    marginTop: '9%',
+    width: 360,
+    height: '10.79%',
+  },
+  caption: {
+    padding: 0,
+    color: 'white',
+    fontFamily: 'Inter-Regular',
+    // maxHeight: 300,
+    fontSize: 16,
+  },
+  shareToContainer: {
+    marginTop: '10%',
+    justifyContent: 'flex-start',
+    width: 360,
+  },
+  shareTo: {
+    color: 'white',
+    fontFamily: 'Inter-Medium',
+    fontSize: 18,
+    marginBottom: '5%',
+  },
+
+  socialRowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: '3%',
+  },
+  socialLeftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  socialIcon: {
+    marginRight: '10%',
+  },
+  socials: {
+    color: 'white',
+    fontSize: 16,
+    fontFamily: 'Inter-Regular',
+  },
+  switch: {
+    height: 100,
+    width: 100,
+  },
+  postBtn: {
+    position: 'absolute',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.red,
+    borderRadius: 9,
+    marginTop: '52%',
+    width: 360,
+    height: 50,
+  },
+  postBtnText: {
+    color: 'white',
+    fontFamily: 'Inter-bold',
+    marginLeft: 10,
+    fontSize: 20,
   },
 });
