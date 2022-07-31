c3545e490e744d42b4cc044b7ee3498cdb21c507
Max Mandia <mmandia511@gmail.com>
2022-07-31 18:14:38 -0400

    Feat: installed libs

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
   useNavigationState,
   useRoute,
 } from '@react-navigation/native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import WelcomeScreen from './src/screens/signup/WelcomeScreen';
 import PhoneNumberScreen from './src/screens/signup/PhoneNumberScreen';
@@ -59,78 +60,84 @@ export default function App() {
   }, [currentTrack, homeScreenFocus]);
 
   return (
-    <NavigationContainer>
-      <Context.Provider
-        value={{
-          confirm,
-          setConfirm,
-          setUserLogin,
-          refreshToken,
-          setRefreshToken,
-          currentTrack,
-          setCurrentTrack,
-          homeScreenFocus,
-          setHomeScreenFocus,
-        }}>
-        {userLogin ? (
-          <Tab.Navigator
-            screenOptions={({route}) => ({
-              tabBarIcon: ({focused, color, size}) => {
-                let iconName;
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <StatusBar barStyle="light-content" />
+        <Context.Provider
+          value={{
+            confirm,
+            setConfirm,
+            setUserLogin,
+            refreshToken,
+            setRefreshToken,
+            currentTrack,
+            setCurrentTrack,
+            homeScreenFocus,
+            setHomeScreenFocus,
+          }}>
+          {userLogin ? (
+            <Tab.Navigator
+              screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                  let iconName;
 
-                if (route.name === 'Home') {
-                  iconName = focused ? 'home-sharp' : 'home-outline';
-                } else if (route.name === 'Discover') {
-                  iconName = focused ? 'ear-sharp' : 'ear-outline';
-                } else if (route.name === 'Post') {
-                  iconName = focused ? 'add-sharp' : 'add-outline';
-                } else if (route.name === 'Activity') {
-                  iconName = focused
-                    ? 'notifications-sharp'
-                    : 'notifications-outline';
-                } else if (route.name === 'Profile') {
-                  iconName = focused ? 'person-sharp' : 'person-outline';
-                }
+                  if (route.name === 'Home') {
+                    iconName = focused ? 'home-sharp' : 'home-outline';
+                  } else if (route.name === 'Discover') {
+                    iconName = focused ? 'ear-sharp' : 'ear-outline';
+                  } else if (route.name === 'Post') {
+                    iconName = focused ? 'add-sharp' : 'add-outline';
+                  } else if (route.name === 'Activity') {
+                    iconName = focused
+                      ? 'notifications-sharp'
+                      : 'notifications-outline';
+                  } else if (route.name === 'Profile') {
+                    iconName = focused ? 'person-sharp' : 'person-outline';
+                  }
 
-                // You can return any component that you like here!
-                return <Ionicons name={iconName} size={size} color={color} />;
-              },
-              tabBarActiveTintColor: 'white',
-              tabBarInactiveTintColor: 'grey',
-              tabBarInactiveBackgroundColor: 'black',
-              tabBarActiveBackgroundColor: 'black',
-              headerShown: false,
-              tabBarStyle: {backgroundColor: 'black'},
-            })}>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Discover" component={test} />
-            <Tab.Screen name="Post" component={PostStackScreen} />
-            <Tab.Screen name="Activity" component={test} />
-            <Tab.Screen name="Profile" component={test} />
-          </Tab.Navigator>
-        ) : (
-          <Stack.Navigator
-            screenOptions={{
-              headerShown: false,
-            }}>
-            <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
-            <Stack.Screen
-              name="PhoneNumberScreen"
-              component={PhoneNumberScreen}
-            />
-            <Stack.Screen name="EnterCodeScreen" component={EnterCodeScreen} />
-            <Stack.Screen
-              name="CreateUsernameScreen"
-              component={CreateUsernameScreen}
-            />
-            <Stack.Screen
-              name="ConnectSpotifyScreen"
-              component={ConnectSpotifyScreen}
-            />
-          </Stack.Navigator>
-        )}
-      </Context.Provider>
-    </NavigationContainer>
+                  // You can return any component that you like here!
+                  return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: 'white',
+                tabBarInactiveTintColor: 'grey',
+                tabBarInactiveBackgroundColor: 'black',
+                tabBarActiveBackgroundColor: 'black',
+                headerShown: false,
+                tabBarStyle: {backgroundColor: 'black'},
+              })}>
+              <Tab.Screen name="Home" component={HomeScreen} />
+              <Tab.Screen name="Discover" component={test} />
+              <Tab.Screen name="Post" component={PostStackScreen} />
+              <Tab.Screen name="Activity" component={test} />
+              <Tab.Screen name="Profile" component={test} />
+            </Tab.Navigator>
+          ) : (
+            <Stack.Navigator
+              screenOptions={{
+                headerShown: false,
+              }}>
+              <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+              <Stack.Screen
+                name="PhoneNumberScreen"
+                component={PhoneNumberScreen}
+              />
+              <Stack.Screen
+                name="EnterCodeScreen"
+                component={EnterCodeScreen}
+              />
+              <Stack.Screen
+                name="CreateUsernameScreen"
+                component={CreateUsernameScreen}
+              />
+              <Stack.Screen
+                name="ConnectSpotifyScreen"
+                component={ConnectSpotifyScreen}
+              />
+            </Stack.Navigator>
+          )}
+        </Context.Provider>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 const styles = StyleSheet.create({
