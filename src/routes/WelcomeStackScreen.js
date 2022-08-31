766a160bc75ade8b2e04425b3ab056c6dc42bdf1
Max Mandia <mmandia511@gmail.com>
2022-08-31 14:42:38 -0400

    Feat: added login screens

---
+++ src/routes/WelcomeStackScreen.js
@@ -0,0 +1,50 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ActivityScreen from '../screens/activity/ActivityScreen';
+import WelcomeScreen from '../screens/signup/WelcomeScreen';
+import PhoneNumberScreen from '../screens/signup/PhoneNumberScreen';
+import EnterCodeScreen from '../screens/signup/EnterCodeScreen';
+import CreateUsernameScreen from '../screens/signup/CreateUsernameScreen';
+import ConnectSpotifyScreen from '../screens/signup/ConnectSpotifyScreen';
+import SwipeUpScreen from '../screens/signup/SwipeUpScreen';
+import SwipeRightScreen from '../screens/signup/SwipeRightScreen';
+import ExistingPhoneNumberScreen from '../screens/signup/ExistingPhoneNumberScreen';
+import ExistingCodeScreen from '../screens/signup/ExistingCodeScreen';
+const WelcomeStackScreen = () => {
+  const WelcomeStack = createNativeStackNavigator();
+  return (
+    <WelcomeStack.Navigator screenOptions={{headerShown: false}}>
+      <WelcomeStack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+
+      <WelcomeStack.Screen
+        name="PhoneNumberScreen"
+        component={PhoneNumberScreen}
+      />
+      <WelcomeStack.Screen name="EnterCodeScreen" component={EnterCodeScreen} />
+      <WelcomeStack.Screen
+        name="CreateUsernameScreen"
+        component={CreateUsernameScreen}
+      />
+      <WelcomeStack.Screen
+        name="ConnectSpotifyScreen"
+        component={ConnectSpotifyScreen}
+      />
+      <WelcomeStack.Screen name="SwipeUpScreen" component={SwipeUpScreen} />
+      <WelcomeStack.Screen
+        name="SwipeRightScreen"
+        component={SwipeRightScreen}
+      />
+      <WelcomeStack.Screen
+        name="ExistingPhoneNumberScreen"
+        component={ExistingPhoneNumberScreen}
+      />
+      <WelcomeStack.Screen
+        name="ExistingCodeScreen"
+        component={ExistingCodeScreen}
+      />
+    </WelcomeStack.Navigator>
+  );
+};
+
+export default WelcomeStackScreen;
---
+++ src/screens/signup/ExistingCodeScreen.js
@@ -0,0 +1,186 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import {useState, useContext, useEffect, useRef} from 'react';
+import {Context} from '../../context/Context';
+import Musicplace from '../../assets/img/musicplace-signup.svg';
+import HapticFeedback from 'react-native-haptic-feedback';
+
+const ExistingCodeScreen = ({navigation}) => {
+  const [showEnterDone, setShowEnterDone] = useState(false);
+  const [verificationCode, setVerificationCode] = useState();
+  const {confirm, setUserLogin} = useContext(Context);
+
+  const goBack = () => {
+    navigation.navigate('ExistingPhoneNumberScreen');
+  };
+
+  const handleInput = text => {
+    const formattedCode = formatCode(text);
+    setVerificationCode(formattedCode);
+  };
+
+  const formatCode = text => {
+    if (!text) {
+      return text;
+    }
+    const code = text.replace(/[^\d]/g, '');
+    const codeLength = code.length;
+    if (codeLength <= 5) {
+      setShowEnterDone(false);
+      return code;
+    }
+    if (codeLength >= 6) {
+      setShowEnterDone(true);
+      return code.slice(0, 6);
+    }
+  };
+
+  let enterCode = async () => {
+    try {
+      HapticFeedback.trigger('impactHeavy');
+      await confirm.confirm(verificationCode);
+      setUserLogin(true);
+    } catch (error) {
+      console.log('Invalid code.');
+      return;
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableWithoutFeedback onPress={goBack}>
+        <View style={styles.touchContainer}>
+          <Image
+            style={styles.chevron}
+            source={require('../../assets/img/ChevronLeft.jpg')}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Musicplace style={styles.musicplace} />
+      <Text style={styles.desc}>Enter your confirmation code.</Text>
+      <View style={styles.inputContainer}>
+        <View style={styles.rectangle}>
+          <TextInput
+            keyboardType="numeric"
+            style={styles.inputText}
+            placeholder="123456"
+            placeholderTextColor="grey"
+            value={verificationCode}
+            onChangeText={text => handleInput(text)}
+            keyboardAppearance="dark"
+            autoFocus="true"
+            textContentType="oneTimeCode"
+          />
+        </View>
+      </View>
+      <View style={styles.nextBtnContainer}>
+        <TouchableOpacity
+          style={showEnterDone ? styles.nextBtnDone : styles.nextBtn}
+          onPress={enterCode}>
+          <Text style={showEnterDone ? styles.nextTextDone : styles.nextText}>
+            Submit
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ExistingCodeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  touchContainer: {
+    width: '15%',
+  },
+  chevron: {
+    marginTop: '1%',
+  },
+  musicplace: {
+    position: 'absolute',
+    alignSelf: 'center',
+    top: '8.75%',
+  },
+  desc: {
+    color: 'white',
+    fontFamily: 'Inter-semibold',
+    fontSize: 18,
+    width: 250,
+    textAlign: 'center',
+    alignSelf: 'center',
+    marginTop: '8%',
+  },
+  inputContainer: {
+    alignItems: 'center',
+    marginTop: '12%',
+  },
+  rectangle: {
+    backgroundColor: '#282828',
+    width: '90%',
+    height: 44,
+    borderRadius: 5,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  areaCode: {
+    color: 'white',
+    marginLeft: 14,
+    fontSize: 16,
+  },
+  verticalLine: {
+    marginLeft: 15,
+  },
+  inputText: {
+    color: 'white',
+    fontSize: 16,
+    marginLeft: 14,
+    width: '90%',
+    fontFamily: 'Inter-Medium',
+    alignSelf: 'center',
+  },
+  nextBtnContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  nextBtn: {
+    backgroundColor: 'rgba(255, 8, 0, 0.5)',
+    borderRadius: 9,
+    paddingVertical: 10,
+    marginTop: '5%',
+    width: '90%',
+  },
+  nextText: {
+    color: 'white',
+    fontFamily: 'Inter-Bold',
+    fontSize: 20,
+    textAlign: 'center',
+    opacity: 0.5,
+  },
+
+  nextBtnDone: {
+    backgroundColor: 'rgb(255, 8, 0)',
+    borderRadius: 9,
+    paddingVertical: 10,
+    marginTop: '5%',
+    width: '90%',
+  },
+
+  nextTextDone: {
+    color: 'white',
+    fontFamily: 'Inter-Bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
