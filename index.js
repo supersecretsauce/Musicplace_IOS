c3812669ed2cac1b657b48b8ac3158189784d8b8
Max Mandia <mmandia511@gmail.com>
2022-11-10 13:28:21 -0500

    feat: integrated notifee for background messages

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
+import notifee from '@notifee/react-native';
 // Register background handler
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('Message handled in the background!', remoteMessage);
-});
+
+function onMessageReceived(message) {
+  notifee.displayNotification(JSON.parse(message.data.notifee));
+}
+
+messaging().setBackgroundMessageHandler(onMessageReceived);
 
 AppRegistry.registerComponent(appName, () => App);
