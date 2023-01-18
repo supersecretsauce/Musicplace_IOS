a5592c35e5c3574860621fbfbf00a6375fe64e06
Max Mandia <mmandia511@gmail.com>
2023-01-18 18:21:27 -0500

    feat: prevented font scaling

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
  */
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
+import {Text, TextInput} from 'react-native';
+
 import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
@@ -18,3 +20,14 @@ messaging().setBackgroundMessageHandler(onMessageReceived);
 messaging().onMessage(onMessageReceived);
 
 AppRegistry.registerComponent(appName, () => App);
+
+//ADD this
+if (Text.defaultProps == null) {
+  Text.defaultProps = {};
+  Text.defaultProps.allowFontScaling = false;
+}
+
+if (TextInput.defaultProps == null) {
+  TextInput.defaultProps = {};
+  TextInput.defaultProps.allowFontScaling = false;
+}
