ec16aefc224666c81102f99c1db9cd8ececadaaf
Max Mandia <mmandia511@gmail.com>
2023-01-19 16:16:51 -0500

    fixed invite preview spacing

--- src/screens/activity/InviteContactsScreen.js
+++ src/screens/activity/InviteContactsScreen.js
@@ -216,7 +216,7 @@ const InviteContactsScreen = ({route, navigation}) => {
             <View style={styles.previewBubble}>
               <Text style={styles.textPreview}>
                 {myName ? myName : myPhoneNumber} invited you to join
-                Musicplace. Your number is now eligible! Download the app
+                Musicplace. Your number is now eligible! Download the app{' '}
                 <Text style={{textDecorationLine: 'underline'}}>here</Text>
               </Text>
             </View>
