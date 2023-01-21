51e59a01fabe442edc403ea177ff86d3e328923d
Max Mandia <mmandia511@gmail.com>
2023-01-21 10:53:26 -0500

    feat: removed client secret from spotconfig

--- SpotifyConfig.js
+++ SpotifyConfig.js
@@ -1,6 +1,5 @@
 export const spotConfig = {
   clientId: '501638f5cfb04abfb61d039e370c5d99', // available on the app page
-  clientSecret: '16f92a6d7e9a4180b29af25bf012e6fe', // click "show client secret" to see this
   redirectUrl: 'musicplace-ios:/musicplace-ios-login', // the redirect you defined after creating the app
   scopes: [
     'playlist-modify-public',
