1204e65c5213ad411eb617dff11352cdc02369a6
Max Mandia <mmandia511@gmail.com>
2023-02-02 19:54:45 -0500

    feat: removed old context providers and files

--- src/routes/SinglePostDrawerRoute.js
+++ src/routes/SinglePostDrawerRoute.js
@@ -1,44 +1,36 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import ViewPostsScreen from '../screens/profile/ViewPostsScreen';
 import SongDrawer from '../components/SongDrawer';
-import {SinglePostContext} from '../context/SinglePostContext';
 const Drawer = createDrawerNavigator();
 
 const SinglePostDrawerRoute = ({route}) => {
   const {songInfo, UID, prevScreen, openSheet, commentDocID, replyRef} =
     route.params ?? {};
-  const [feedTrack, setFeedTrack] = useState(null);
   return (
-    <SinglePostContext.Provider
-      value={{
-        feedTrack,
-        setFeedTrack,
+    <Drawer.Navigator
+      drawerContent={props => <SongDrawer {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerPosition: 'right',
+        drawerStyle: {
+          width: '84%',
+        },
+        swipeEnabled: false,
       }}>
-      <Drawer.Navigator
-        drawerContent={props => <SongDrawer {...props} />}
-        screenOptions={{
-          headerShown: false,
-          drawerPosition: 'right',
-          drawerStyle: {
-            width: '84%',
-          },
-          swipeEnabled: false,
-        }}>
-        <Drawer.Screen
-          name="ViewPostsScreen"
-          component={ViewPostsScreen}
-          initialParams={{
-            songInfo: songInfo,
-            UID: UID,
-            prevScreen: prevScreen,
-            openSheet: openSheet,
-            commentDocID: commentDocID,
-            replyRef: replyRef,
-          }}
-        />
-      </Drawer.Navigator>
-    </SinglePostContext.Provider>
+      <Drawer.Screen
+        name="ViewPostsScreen"
+        component={ViewPostsScreen}
+        initialParams={{
+          songInfo: songInfo,
+          UID: UID,
+          prevScreen: prevScreen,
+          openSheet: openSheet,
+          commentDocID: commentDocID,
+          replyRef: replyRef,
+        }}
+      />
+    </Drawer.Navigator>
   );
 };
 
