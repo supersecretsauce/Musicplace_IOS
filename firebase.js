f08a22a44447a3f2d2277221b0decc23f48a02a4
Max <89795178+maxmandia@users.noreply.github.com>
2023-06-12 16:47:30 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,15 +7,7 @@ import {getFirestore} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyCSy_UcR1Il6WncIH4Pdz_fuhugPfulhVU',
-  authDomain: 'musicplace-66f20.firebaseapp.com',
-  projectId: 'musicplace-66f20',
-  storageBucket: 'musicplace-66f20.appspot.com',
-  messagingSenderId: '642367062887',
-  appId: '1:642367062887:web:bd03a4424700e95f0265c2',
-  measurementId: 'G-B3W0REHP2T',
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
